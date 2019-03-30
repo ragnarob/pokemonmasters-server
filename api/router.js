72cbaeb26d7d96542ffbb7cc7dda62ced6857ca7
ragnarob <ragnarob@protonmail.com>
2019-03-30 14:54:24 +0100

    Implement /api/pokemon, finish /api/joingame

--- api/router.js
+++ api/router.js
@@ -9,8 +9,6 @@ module.exports = class Router {
 		this.app = app
 		this.setupRoutes()
 
-		this.addPlayerToGame()
-
 		this.app.listen(3000)
 	}
 
@@ -21,6 +19,7 @@ module.exports = class Router {
 		this.app.post('/api/gamestatus', this.getGameStatus)
 		this.app.post('/api/createteam', this.createTeam)
 		// osv? Kanskje splitte i to modules, en for game-relaterte ting og en for /moves, /pokemon, osv, altså static content
+		this.app.get ('/api/pokemon', this.getAllPokemon.bind(this))
 	}
 
 	hello (req, res) {
@@ -45,8 +44,8 @@ module.exports = class Router {
 	}
 
 	async addPlayerToGame (req, res) {
-		// let [playerName, gameCode] = [req.body.playerName, req.body.gameCode]
-		let [playerName, gameCode] = ['rag', '5PVNJ']
+		console.log(req.body)
+		let [playerName, gameCode] = [req.body.playerName, req.body.gameCode]
 
 		try {
 			let gameInstance = await GameInstance.findOne({gameCode: gameCode})
@@ -57,10 +56,11 @@ module.exports = class Router {
 				ready: false,
 				message: '',
 				hasWinner: false,
-				gameState: gameInstance.playerNames.map(p => ({p: []}))
+				gameState: gameInstance.playerNames.map(name => ({playerName: name, pokemon: []}))
 			}
 			
-			console.log(gameInstance)
+			gameInstance.save()
+			res.json({gameToken: gameInstance.gameToken})
 		}
 		catch (err) {
 			res.json({error: 'Some error'})
@@ -82,15 +82,14 @@ module.exports = class Router {
 
 		let [gameToken, playerName, pokemonList] = [req.body.gameToken, req.body.playerName, req.body.pokemonList]
 		try {
-			let gameInstance = GameInstance.findOne({gameToken: gameToken})
+			let gameInstance = await GameInstance.findOne({gameToken: gameToken})
 			if (!gameInstance) { res.json({error: 'Invalid game token'}) }
 			
 			for (var i=0; i<pokemonList.length; i++) {
-				let pokemonData = this.pokemon[pokemonList[i].name]
+				let pokemonData = this.pokemon[pokemonList[i].name.toLowerCase()]
 				let pokemonMoves = pokemonList[i].moves
-
 				let inGamePokemon = new InGamePokemon({
-					name: pokemonData.name,
+					name: pokemonData.species,
 					types: pokemonData.types,
 					baseStats: new Map(Object.entries(pokemonData.baseStats)),
 					stats: new Map(Object.entries(pokemonData.baseStats)),
@@ -105,10 +104,27 @@ module.exports = class Router {
 			if (gameInstance.state.gameState.every(gs => gs.pokemon.length > 0)) {
 				gameInstance.gameStage = 2
 			}
+			res.json({status: 'success'})
 		}
 		catch (err) {
-			console.log(err)
 			res.json({error: 'Some error'})
 		}
+	}
+
+	getAllPokemon (req, res) {
+		let pokemonWithMoves = []
+		for (let pokemonKey in this.pokemon) {
+			let pokemon = this.pokemon[pokemonKey]
+			console.log(pokemon)
+			pokemon.detailedMoves = []
+			for (let moveKey in this.moves) {
+				if (pokemon.moves.indexOf(moveKey) > -1) {
+					pokemon.detailedMoves.push(this.moves[moveKey])
+				}
+			}
+			pokemonWithMoves.push(pokemon)
+		}
+
+		res.json({pokemon: pokemonWithMoves})
 	}
 }
